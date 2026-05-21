91dfe6a7d100855f96f1397dd547835332a09ec7
cda67730 <141915863+cda67730@users.noreply.github.com>
2026-05-21 21:38:34 +0800

    Add files via upload

--- fastapi_app_cloud.py
+++ fastapi_app_cloud.py
@@ -382,7 +382,6 @@ class DatabaseQuery:
             '00980A': '主動野村臺灣優選ETF',
             '00981A': '統一台股增長主動式ETF',
             '00982A': '群益台灣精選強棒主動式ETF',
-            '00983A': '中信ARK創新主動式ETF',
             '00984A': '安聯台灣高股息成長主動式ETF',
             '00985A': '野村台灣增強50主動式ETF',
             '00991A': '復華未來50主動式ETF',
@@ -391,6 +390,9 @@ class DatabaseQuery:
             '00994A': '第一金台股趨勢優選主動式ETF',
             '00995A': '中信台灣卓越主動式ETF',
             '00403A': '統一台股升級50主動式ETF',
+            '00996A': '兆豐台灣豐收主動式ETF',
+            '00998A': '復華金融股息主動式ETF',
+            '00999A': '野村臺灣策略高息主動式ETF',
         }
         self.db_available = db_config is not None
         if self.db_available:
